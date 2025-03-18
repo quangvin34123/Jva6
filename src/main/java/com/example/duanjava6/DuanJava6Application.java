467f412632289e1d2e4fa0fef6f38a6e8240c1ea
quangvin34123 <vinhlqpd10257@gmail.com>
2025-03-19 00:44:48 +0700

    java6

---
+++ src/main/java/com/example/duanjava6/DuanJava6Application.java
@@ -0,0 +1,13 @@
+package com.example.duanjava6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuanJava6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuanJava6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/duanjava6/DuanJava6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.duanjava6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuanJava6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
