41d79546d1d8fc13922b33a9b07c5a23a6ca370b
quangvin34123 <vinhlqpd10257@gmail.com>
2025-04-03 20:04:44 +0700

    Add files via upload

---
+++ CartItem.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.example.duanjava6.Controller;
+
+import java.util.stream.DoubleStream;
+
+
+class CartItem {
+
+    void setQuantity(int quantity) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    Object getName() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    DoubleStream getPrice() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    DoubleStream getQuantity() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
